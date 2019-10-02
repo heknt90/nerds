97c911f99968f98f568234a93c62eef49d31abf0
Nikita Efremov <heknt90@gmail.com>
2019-10-02 21:08:57 +0300

    Поведение элементов меню, поведение логотипа, автопроигрывание promo-sliderа

---
+++ source/js/script.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// >> Slider
+
+var slider = {
+  checkboxes: document.querySelectorAll('.promo-slider__checkbox'),
+  currentSlide: 0,
+  timeWait: 4000
+}
+
+if (slider.checkboxes.length > 0) {
+  var autoplayPromoSlider = setInterval(function() {
+    setSlide(slider.currentSlide + 1);
+  }, slider.timeWait);
+
+  for (var i = 0; i < slider.checkboxes.length; i++) {
+    slider.checkboxes[i].addEventListener('change', function() {
+      clearInterval(autoplayPromoSlider);
+    });
+  }
+}
+
+function setSlide(slideNumber) {
+  var nextSlide = Math.abs((slideNumber + slider.checkboxes.length) % slider.checkboxes.length);
+  slider.checkboxes[nextSlide].checked = true;
+  slider.currentSlide = nextSlide;
+  console.log(slider.currentSlide);
+  // return slider.currentSlide;
+}
